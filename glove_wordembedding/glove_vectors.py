17e232f7953977cda8c2e3b4f360bd23feef2e21
pemagrg1 <pema.grg1@gmail.com>
2019-09-10 16:25:47 +0545

    word embedding using glove

---
+++ glove_wordembedding/glove_vectors.py
@@ -0,0 +1,22 @@
+#####-------------GLOVE------------
+import codecs
+import numpy as np
+
+def load_glove_model(gloveFile):
+    #print "Loading Glove Model"
+    f = codecs.open(gloveFile,'r', encoding='utf-8')
+    model = {}
+    for line in f:
+        splitLine = line.split()
+        word = splitLine[0]
+        embedding = np.array([float(val) for val in splitLine[1:]])
+        model[word] = embedding
+    #print "Done.",len(model)," words loaded!"
+    return model
+
+#------Load the model
+glove = load_glove_model('/media/ekbana/ekbana500/glove.6B.100d.txt')
+#------transform simple text to vectors
+text = "hello!! my name is Pema"
+vectors= glove.get(text, np.zeros(50))
+print (vectors)
