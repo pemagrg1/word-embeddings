b83c10695a9b9d38a1d053e6f6aeb0f963f0ec58
pemagrg1 <pema.grg1@gmail.com>
2019-11-11 15:57:46 +0545

    adding new data

--- modules/gensim_wordembedding/gensim_w2v.py
+++ modules/gensim_wordembedding/gensim_w2v.py
@@ -1,6 +1,20 @@
 import gensim
-gensim_model = "/media/ekbana/ekbana500/Pema/datasets/GoogleNews-vectors-negative300.bin.gz"
-gensim_model = gensim.models.KeyedVectors.load_word2vec_format(gensim_model, binary=True, limit=100000)
-gensim_vector = gensim_model['dog']
-print(gensim_vector.shape)
-print (gensim_vector)
+
+class gensim_mode:
+    def __init__(self):
+        pass
+
+    def train_csv(self):
+        pass
+
+    def train_text(self):
+        pass
+
+    def load_savedModel(self):
+        gensim_model = "/media/ekbana/ekbana500/Pema/datasets/GoogleNews-vectors-negative300.bin.gz"
+        gensim_model = gensim.models.KeyedVectors.load_word2vec_format(
+            gensim_model, binary=True, limit=100000)
+        gensim_vector = gensim_model['dog']
+        print(gensim_vector.shape)
+        print(gensim_vector)
+
